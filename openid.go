892faa7f33358017145632411cc85e6642147cc9
coldWater <forsaken628@gmail.com>
2017-08-26 18:41:04 +0800

    PutVarint

--- openid.go
+++ openid.go
@@ -2,13 +2,15 @@ package openid
 
 import (
 	"crypto/aes"
-	"crypto/rand"
 	"crypto/cipher"
-	"encoding/binary"
-	"encoding/base32"
-	"errors"
 	"crypto/md5"
+	"crypto/rand"
+	"encoding/base32"
+	"encoding/binary"
+	"errors"
 )
+
+var ErrInvalidOpenid = errors.New("invalid openid")
 
 type Source struct {
 	key   []byte
@@ -24,75 +26,78 @@ func New(key []byte) *Source {
 }
 
 func (s *Source) Encode(src int64) string {
-	salt := [2]byte{}
+	salt := [5]byte{}
 	_, err := rand.Read(salt[:])
 	if err != nil {
 		panic(err)
 	}
-	return s.encode(src, salt)
+	return s.EncodeWithSalt(src, salt)
 }
 
-func (s *Source) EncodeStatic(src int64, region string) string {
-	salt := [2]byte{}
+func (s *Source) EncodeRegion(src int64, region string) string {
+	salt := [5]byte{}
+	_, err := rand.Read(salt[:4])
+	if err != nil {
+		panic(err)
+	}
 	sum := md5.Sum([]byte(region))
-	copy(salt[:], sum[:2])
-	return s.encode(src, salt)
+	salt[4] = sum[0]
+	return s.EncodeWithSalt(src, salt)
 }
 
-func (s *Source) EncodeWithSalt(src int64, salt [2]byte) string {
-	return s.encode(src, salt)
-}
-
-func (s *Source) encode(src int64, salt [2]byte) string {
-	buf1 := [10]byte{}
-	buf2 := [8]byte{}
-	copy(buf1[8:10], salt[:])
-
-	binary.LittleEndian.PutUint64(buf2[:], uint64(src))
-
-	iv := [aes.BlockSize]byte{buf1[8], buf1[9]}
-	t := buf1[9]&0x07 + 1
+func (s *Source) EncodeWithSalt(src int64, salt [5]byte) string {
+	buf := [15]byte{}
+	iv := [aes.BlockSize]byte{}
+	copy(buf[:5], salt[:])
+	copy(iv[:5], salt[:])
+	t := salt[0]&0x07 + 1
 	for i := byte(0); i < t; i++ {
 		s.block.Encrypt(iv[:], iv[:])
 	}
 
-	offset := iv[9] & 0x70 >> 4
-	for i := byte(0); i < 8; i++ {
-		if i+offset < 8 {
-			buf1[i] = iv[i] ^ buf2[i+offset]
-		} else {
-			buf1[i] = iv[i] ^ buf2[i+offset-8]
-		}
+	n := binary.PutVarint(buf[5:], src)
+	if n < 5 {
+		n = 5
 	}
 
-	return base32.StdEncoding.EncodeToString(buf1[:])
+	for i := byte(0); i < byte(n); i++ {
+		buf[5+i] ^= iv[i]
+	}
+
+	return base32.StdEncoding.EncodeToString(buf[:5+n])
 }
 
 func (s *Source) Decode(src string) (int64, error) {
-	buf1, err := base32.StdEncoding.DecodeString(src)
-	buf2 := [8]byte{}
+	return s.decode(src, nil)
+}
+
+func (s *Source) DecodeRegion(src string, region string) (int64, error) {
+	sum := md5.Sum([]byte(region))
+	return s.decode(src, &sum[0])
+}
+
+func (s *Source) decode(src string, regionHash *byte) (int64, error) {
+	buf, err := base32.StdEncoding.DecodeString(src)
 	if err != nil {
 		return 0, err
 	}
 
-	if len(buf1) != 10 {
-		return 0, errors.New("invalid src")
+	if regionHash != nil && *regionHash != buf[4] {
+		return 0, ErrInvalidOpenid
 	}
 
-	iv := [aes.BlockSize]byte{buf1[8], buf1[9]}
-	t := buf1[9]&0x07 + 1
+	iv := [aes.BlockSize]byte{}
+	copy(iv[:], buf[:5])
+	t := buf[0]&0x07 + 1
 	for i := byte(0); i < t; i++ {
 		s.block.Encrypt(iv[:], iv[:])
 	}
 
-	offset := iv[9] & 0x70 >> 4
-	for i := byte(0); i < 8; i++ {
-		if i+offset < 8 {
-			buf2[i+offset] = iv[i] ^ buf1[i]
-		} else {
-			buf2[i+offset-8] = iv[i] ^ buf1[i]
-		}
+	n := byte(len(buf))
+	for i := byte(5); i < n; i++ {
+		buf[i] ^= iv[i-5]
 	}
 
-	return int64(binary.LittleEndian.Uint64(buf2[:])), nil
+	x, _ := binary.Varint(buf[5:])
+	return x, nil
 }
--- openid_test.go
+++ openid_test.go
@@ -1,19 +1,27 @@
 package openid
 
 import (
-	"testing"
 	"math"
+	"testing"
 )
 
-var srcs = []int64{0, 1, 5, 10, 1000, 3453453, 34345345345, -1, -34234, math.MaxInt64, -math.MaxInt64 - 1, math.MaxInt32}
+var i64s = []int64{
+	0, 1, -1, 5, -5,
+	10, -10, 1000, -1000,
+	1234567,
+	12345678901,
 
-func TestSource_Encode(t *testing.T) {
-	key := make([]byte, 16)
-	key[0] = 5
-	s := New(key)
-	for _, src1 := range srcs {
+	1234567890123,
+	123456789012345,
+	math.MaxInt64,
+	-math.MaxInt64 - 1}
+
+func TestEncode(t *testing.T) {
+	key := [32]byte{5}
+	s := New(key[:])
+	for _, src1 := range i64s {
 		str := s.Encode(src1)
-		// t.Log(str)
+		t.Log(str)
 		src2, err := s.Decode(str)
 		if err != nil {
 			t.Fatal(err)
@@ -21,5 +29,18 @@ func TestSource_Encode(t *testing.T) {
 		if src1 != src2 {
 			t.Fatal(src1, src2)
 		}
+	}
+}
+
+func TestEncodeRegion(t *testing.T) {
+	key := [16]byte{37, 45}
+	s := New(key[:])
+	_, err := s.DecodeRegion(s.EncodeRegion(12312, "adsf"), "adsf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = s.DecodeRegion(s.EncodeRegion(12312, "adsf"), "ads")
+	if err != ErrInvalidOpenid {
+		t.Fatal(err)
 	}
 }
